988c83355a6873d77f5ac40eaec1adc6904e0ab3
komari <foundationscp11@gmail.com>
2025-08-04 14:26:35 +0500

    new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.SkrinVex.OfoxMessenger"
         minSdk = 24
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.0"
+        versionCode = 24
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
